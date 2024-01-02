4848c7015912430a15e42ddacd70b54f5f63256b
Mustafa Uçar <mustafa19071907mustafa@gmail.com>
2024-01-02 18:26:29 +0300

    Add new files and update data.csv and ml.py

--- ml.py
+++ ml.py
@@ -1,10 +1,11 @@
 import pandas as pd
 import numpy as np
 from sklearn.compose import ColumnTransformer
-from sklearn.preprocessing import OneHotEncoder , LabelEncoder
+from sklearn.preprocessing import LabelEncoder , StandardScaler
 from sklearn.linear_model import LinearRegression
 
 def fiyat_tahmini(user_input):
+    print("user_input", user_input)
     # CSV dosyasını oku
     df = pd.read_csv('data.csv')
 
@@ -12,40 +13,42 @@ def fiyat_tahmini(user_input):
     data_selected = df[['price', 'status']]
 
     # Bağımsız değişkenleri ve bağımlı değişkeni tanımla
-    X = data_selected.drop('price', axis=1)
-    y = data_selected['price']
+    X = data_selected.drop('price', axis=1).values
+    y = data_selected['price'].values
 
     # Kategorik verileri sayısal verilere dönüştür
     le = LabelEncoder()
-    X['status'] = le.fit_transform(X['status'])
-    # Kategorik verileri one-hot encoding ile sayısal verilere dönüştür
-    ohe = ColumnTransformer([('ohe', OneHotEncoder(dtype=float), [0])], remainder='passthrough')
-    X = ohe.fit_transform(X)
+    X[:, 0] = le.fit_transform(X[:, 0])
 
-    # Modeli eğit
+
+    # Verileri ölçeklendir
+    sc = StandardScaler()
+    y = y.reshape(-1, 1)
+    y = sc.fit_transform(y)
+
     model = LinearRegression()
     model.fit(X, y)
-    
-    # Kullanıcı girişini DataFrame'e çevir
-    user_input_df = pd.DataFrame(user_input, index=[0])
 
-    # Kullanıcı girişindeki kategorik verileri sayısal verilere dönüştür
+    # Kullanıcı girişini DataFrame'e dönüştür
+    user_input_df = pd.DataFrame(data=user_input, index=[0])
+
+    # Kategorik verileri sayısal verilere dönüştür
     user_input_df['status'] = le.transform(user_input_df['status'])
-
-    # Kullanıcı girişindeki kategorik verileri one-hot encoding ile sayısal verilere dönüştür
-    user_input_df = ohe.transform(user_input_df)
-
 
     # Tahmin yap
     tahmin = model.predict(user_input_df)
 
-    return tahmin[0]
+    # Tahmini ölçeklendirmeyi geri al
+    tahmin = sc.inverse_transform(tahmin)
+
+    print("tahmin", tahmin)
+
+    return tahmin[0][0]
 
 
 # # Örnek kullanıcı girişi
 # kullanici_girisi = {
 #     'status': 'used',
-#     'name': 'Nike Air Max',
 # }
 
 # # Fonksiyonu çağırarak fiyat tahmini yap
